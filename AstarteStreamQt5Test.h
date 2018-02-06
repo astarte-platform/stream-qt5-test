476d98b889068e95239cbce4f8f4f8be6b9670a4
Davide Bettio <davide.bettio@ispirata.com>
2018-02-06 17:05:49 +0100

    Add application code
    Implement simple function generation and stream to Astarte.

---
+++ AstarteStreamQt5Test.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2018 Ispirata Srl
+ *
+ * This file is part of Astarte.
+ * Astarte is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Astarte is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Astarte.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ASTARTESTREAMQT5TEST_H
+#define ASTARTESTREAMQT5TEST_H
+
+#include <QtCore/QObject>
+
+namespace Hemera {
+class Operation;
+}
+
+class QTimer;
+
+class AstarteDeviceSDK;
+
+class AstarteStreamQt5Test : public QObject
+{
+    Q_OBJECT
+
+public:
+    AstarteStreamQt5Test(const QByteArray &interface, const QByteArray &path, const QString &function, const QString &device, int interval = 1, double scale = 1, QObject *parent = nullptr);
+    ~AstarteStreamQt5Test();
+
+private slots:
+    void checkInitResult(Hemera::Operation *op);
+    void sendValues();
+
+    void handleIncomingData(const QByteArray &interface, const QByteArray &path, const QVariant &value);
+
+private:
+    AstarteDeviceSDK *m_sdk;
+    QByteArray m_interface;
+    QByteArray m_path;
+    QString m_function;
+    QTimer *m_uptimeTimer;
+    double m_scale;
+    double m_xValue;
+    bool m_ready;
+};
+
+#endif
--- README.md
+++ README.md
@@ -2,3 +2,20 @@ Astarte Stream Qt5 Test
 =======================
 
 Astarte Qt5 SDK based data stream test.
+
+Requirements
+============
+
+* Astarte Qt5 SDK and Qt5 devel tools
+
+Getting started
+===============
+
+Create a configuration directory using the given template (astarte-device-fbZ1F-TUpH1O7ryuLsGTzw-conf in this example) and customize it.
+
+Build and run the application using following commands:
+```
+$ qmake .
+$ make
+./stream-qt5-test --device gbZ1F-TUpH1O7ryuLsGTzw --function sin --interval 100 --scale 0.0001
+```
