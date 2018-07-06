151f4787bfb3ca23e8d0d5cead1c53e94222d525
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-07-06 15:04:47 +0200

    Handle new pairing endpoint, add REALM env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ mkdir astarte-device-$DEVICE_ID-conf
 cat > astarte-device-$DEVICE_ID-conf/transport-astarte.conf <<EOF
 [AstarteTransport]
 agentKey=$AGENT_KEY
-endpoint=$PAIRING_HOST/api/v1
+endpoint=$PAIRING_HOST/v1/$REALM
 persistencyDir=$PERSISTENCY_DIR
 ignoreSslErrors=$IGNORE_SSL_ERRORS
 EOF
