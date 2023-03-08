a2eb790d2a60815956865a4718d907a57f181766
Antonio Gisondi <antonio.gisondi@secomind.com>
2023-03-08 17:16:09 +0100

    Allow to use credentialsSecret instead of pairingJwt
    Bump Astarte Device SDK to v1.1.0
    
    Signed-off-by: Antonio Gisondi <antonio.gisondi@secomind.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,14 +18,24 @@ fi
 
 # Configure the device directory
 mkdir astarte-device-$DEVICE_ID-conf
-cat > astarte-device-$DEVICE_ID-conf/transport-astarte.conf <<EOF
+TRANSPORT_CONF_PATH="astarte-device-${DEVICE_ID}-conf/transport-astarte.conf"
+cat > $TRANSPORT_CONF_PATH <<EOF
 [AstarteTransport]
-pairingJwt=$PAIRING_JWT
 pairingUrl=$PAIRING_URL
 realm=$REALM
 persistencyDir=$PERSISTENCY_DIR
 ignoreSslErrors=$IGNORE_SSL_ERRORS
 keepAliveSeconds=$KEEPALIVE
 EOF
+
+# Check if CREDENTIALS_SECRET and PAIRING_JWT are both set, print warning
+if [[ -v CREDENTIALS_SECRET && -v PAIRING_JWT ]]; then
+    echo "CREDENTIALS_SECRET and PAIRING_JWT both set, CREDENTIALS_SECRET will be preferred."
+    echo "credentialsSecret=$CREDENTIALS_SECRET" >> $TRANSPORT_CONF_PATH
+elif [[ -v CREDENTIALS_SECRET ]]; then
+    echo "credentialsSecret=$CREDENTIALS_SECRET" >> $TRANSPORT_CONF_PATH
+else
+    echo "pairingJwt=$PAIRING_JWT" >> $TRANSPORT_CONF_PATH
+fi
 
 exec "$@"
